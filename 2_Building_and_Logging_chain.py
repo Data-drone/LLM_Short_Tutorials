7f1c48e9e81156da4541c110592cef0387f289fa
Data-drone <bpl.law@gmail.com>
2024-09-10 17:51:32 +1000

    updated tutorial with latest mlflow and pyfunc process added a basic chain as well

--- 2_Building_and_Logging_chain.py
+++ 2_Building_and_Logging_chain.py
@@ -1,29 +1,104 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC
-# MAGIC # Building and Logging Our Chain
+# MAGIC # Building and Logging A Basic Chain
 # MAGIC
-# MAGIC How to set it up on Databricks
+# MAGIC Setting Up and Logging PyFuncs
+# MAGIC
+# MAGIC Prereqs
+# MAGIC - Note that we assume that the vector index is up and populated already
 
 # COMMAND ----------
 
-# MAGIC %pip install typing_extensions langchain==0.1.16 databricks-vectorsearch==0.27
+# MAGIC %pip install mlflow-skinny==2.16.0 databricks-vectorsearch langchain==0.2.11 langchain_core langchain_community==0.2.10 langchain-databricks
 # MAGIC dbutils.library.restartPython()
 
 # COMMAND ----------
 import mlflow
 
+## configure these to suit what you have available
 chat_model = 'data'
 
 llm_model_name = 'databricks-dbrx-instruct'
 embedding_model = 'databricks-bge-large-en'
 
 catalog = 'brian_gen_ai'
-schema = 'lab_05'
+schema = 'chain_types'
 volumes = 'source_files'
 
 vs_endpoint = 'one-env-shared-endpoint-5'
-vs_index_fullname = f'{catalog}.{schema}.adv_index'
+vs_index_fullname = f'{catalog}.lab_05.arxiv_parse_bge_index'
+
+# In case missing need to create - NOTE doesn't create the vector index
+spark.sql(f"CREATE CATALOG IF NOT EXISTS {catalog}")
+spark.sql(f"CREATE SCHEMA IF NOT EXISTS {catalog}.{schema}")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Creating a Basic chat application
+# MAGIC
+# MAGIC *NOTE* When we want to save out message history, we should do that outside of the chain logic.
+# MAGIC
+
+# COMMAND ----------
+
+class MlflowPyFuncBasicModel(mlflow.pyfunc.PythonModel):
+
+    def __init__(self, llm_model = 'databricks-meta-llama-3-1-70b-instruct'):
+        
+        """
+        The init function is purely to allow for flexibility in initialising and testing
+        It is not needed in the PyFunc spec 
+        """
+
+        self.llm_model = llm_model
+
+    def load_context(self, context):
+
+        from langchain_community.chat_models import ChatDatabricks
+        from langchain_core.prompts import ChatPromptTemplate
+        from langchain_core.output_parsers import StrOutputParser
+        from langchain_core.runnables import RunnablePassthrough
+
+        self.llm_model = ChatDatabricks(
+            target_uri='databricks',
+            endpoint=self.llm_model,
+            temperature=0.1
+        )
+
+    def process_row(self, row):
+       # row['session_id']
+       return self.llm_model.invoke(row['prompt'])
+                                 #config={"configurable": {"session_id": "abc123"}})
+    
+    def predict(self, context, data):
+        results = data.apply(self.process_row, axis=1) 
+
+        # remove .content if it is with Databricks
+        results_text = results.apply(lambda x: x)
+        return results_text
+
+
+# COMMAND ----------
+
+import pandas as pd
+
+basic_model = MlflowPyFuncBasicModel()
+basic_model.load_context()
+
+inference_dataset = pd.DataFrame({'prompt': ['Hi How are you?']})
+
+result_frame = basic_model.predict(data=inference_dataset)
+
+print(result_frame[0].content)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Chain with Vector Search Retriever
+# MAGIC
+# MAGIC Lets look at a chain with a vector store retriever
 
 
 # COMMAND ----------
@@ -80,35 +155,11 @@ class MlflowLangchainwVectorStore(mlflow.pyfunc.PythonModel):
         basic_template = ChatPromptTemplate.from_messages(
         [
             ("system", """
-            You are a helpful assistant, named betGPT, that takes a conversation between a [TAB] customer or employee and yourself 
-            and answer their questions based on the provided context. 
-            Only provide information that is related to [TAB] and its products and give answers from the provided Terms and Conditions documents, 
-            Gambling Code of Practice documents, National Responsible Gambling Messaging document and the TAB Help Centre. 
-             Use the [guidelines] to help formulate your answers.
-
-            It is very important that you NEVER respond saying you are an AI developed by Open AI as you are working for 
-             the TAB corporation and it does not make sense to tell users you are from another company.
-            If you respond incorrectly then you have failed to play the role of a helpful assistant named betGPT.
+            You are a helpful assistant designed to help customers undertake research on RAG models
             
             [Context]
             {rag_content}
 
-            [TAB]
-            TAB is a premier online betting platform, offering a wide range of sports and racing betting 
-            options with a focus on user experience and security. Its interface allows for easy navigation
-            and real-time betting, enhancing user engagement and enjoyment.
-            Committed to promoting responsible gambling, TAB adheres strictly to the Terms and Conditions, 
-             Gambling Code of Practice (as set by the relevant regulatory jurisdiction) and National Responsible Gambling Guide, 
-             providing tools and resources to help users bet responsibly.
-            This dedication ensures a safe, reliable, and fair betting environment, upholding the integrity 
-             of the betting industry. TAB stands out as a trusted platform for betting enthusiasts, balanced by responsible gambling practices.
-            
-            [guidelines]
-            1.Only answer questions related to TAB and its products.
-            2.Do not provide any advice on gambling or encourage the user to gamble.
-            3.If you do not know an answer to a question, say you don't know the answer. Answers should only contain information found from the provided documents.
-            4.Include information about the TAB App and TAB Website in your answer, if it is applicable to the question.
-            5.You are not AI developed by OpenAI. You are a helpful assistant named betGPT.
             """),
             ("human", "{user_input}"),
          ]
